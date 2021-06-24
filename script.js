5711c445f39cd98dd4f672083219573833b762e7
Jenna Leonard <jennaleonard@Jennas-MacBook-Pro.local>
2021-06-24 10:12:09 -0700

    Created a brush.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+// let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+let detailX; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255);
+  background(255);
+  frameRate(40);
+  rectMode(CENTER);
 }
 
 function draw() {
   noStroke();
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  fill(0, 50)
+  // let step = frameCount % 20;
+  // let angle = map(step, 0, 20, 0, TWO_PI);
+  // let cos_a = cos(angle);
+  // let sin_a = sin(angle);
+  // translate(50, 50);
+  // Equivalent to rotate(angle);
+  // applyMatrix(cos_a, sin_a, -sin_a, cos_a, 0, 0);
+  // fill(random(colorlist));
+  rect(mouseX, mouseY, 75, 55, 25);
 }
