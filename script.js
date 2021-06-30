8d1ef7503beed13a8b7a2eaa5c057c02ff534e9f
jennaleonard <jleonardweb@gmail.com>
2021-06-30 00:02:03 -0700

    Added angle rotation, gradient, and erase feature to JS file.

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-// let colorlist = ['gold', 'yellow', 'turquoise', 'red']
-let detailX; 
+let angle = 0;
+let color1;
+let color2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background(0);
   frameRate(40);
   rectMode(CENTER);
+  angleMode(DEGREES);
+  color1 = color(255, 0, 0, 70);
+  color2 = color(0, 0, 255, 70);
 }
 
 function draw() {
-  noStroke();
-  fill(0, 50)
-  // let step = frameCount % 20;
-  // let angle = map(step, 0, 20, 0, TWO_PI);
-  // let cos_a = cos(angle);
-  // let sin_a = sin(angle);
-  // translate(50, 50);
-  // Equivalent to rotate(angle);
-  // applyMatrix(cos_a, sin_a, -sin_a, cos_a, 0, 0);
-  // fill(random(colorlist));
-  rect(mouseX, mouseY, 75, 55, 25);
+  stroke(255,215,0);
+  strokeWeight(1);
+  var percentage = map(mouseY, 0, height, 0, 1);
+  var color = lerpColor(color1, color2, percentage);
+  fill(color);
+
+  push();
+  translate(mouseX, mouseY);
+  rotate(angle);
+  rect(0, 0, 75, 55, 25);
+  pop();
+  
+  angle = angle + 3;
+}
+
+function mouseClicked() {
+  background(0);
 }
